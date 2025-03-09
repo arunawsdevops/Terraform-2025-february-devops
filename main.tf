e437eacd99f6efa847db9e902b5e39476271274a
Renjith B <renjithsre@gmail.com>
2025-03-09 09:37:42 +0800

    uploading the variable

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+     resource "aws_vpc" "renjithvpc" {
+       cidr_block = var.vpc_cidr
+}
+     resource "aws_s3_bucket" "test1123" {
+  bucket = var.aws_s3_bucket
 }
